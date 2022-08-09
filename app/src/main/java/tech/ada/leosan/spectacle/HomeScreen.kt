bab7cd32f2cd91ba4b644fd59348c74e365ce1b4
Leonardo Santana <leonardossnt@gmail.com>
2022-08-09 16:34:32 -0400

    Add initial code for HomeScreen

---
+++ app/src/main/java/tech/ada/leosan/spectacle/HomeScreen.kt
@@ -0,0 +1,9 @@
+package tech.ada.leosan.spectacle
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Text("Hello! Logged in!")
+}
