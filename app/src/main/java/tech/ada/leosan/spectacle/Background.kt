f22ab9ef92b0b78978aa6515d68fb637086af4d4
Leonardo Santana <leonardossnt@gmail.com>
2022-08-08 21:51:55 -0400

    Add Login screen with Compose

---
+++ app/src/main/java/tech/ada/leosan/spectacle/Background.kt
@@ -0,0 +1,29 @@
+package tech.ada.leosan.spectacle
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.res.colorResource
+
+@Composable
+fun Background() {
+    Surface {
+        Box(
+            Modifier
+                .fillMaxSize()
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            MaterialTheme.colors.secondary,
+                            MaterialTheme.colors.primary,
+                        )
+                    )
+                )
+        ) {}
+    }
+}
--- app/src/main/java/tech/ada/leosan/spectacle/LoginActivity.kt
+++ app/src/main/java/tech/ada/leosan/spectacle/LoginActivity.kt
@@ -1,11 +1,143 @@
 package tech.ada.leosan.spectacle
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
-class LoginActivity : AppCompatActivity() {
+
+class LoginActivity : ComponentActivity() {
+    val TAG: String = "LoginActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        setContent {
+            LoginScreen()
+        }
+    }
+
+    @Composable
+    fun LoginScreen() {
+        MaterialTheme(
+            colors = CustomColors.Colors
+        ) {
+            Surface(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Background()
+                LoginTitle()
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.fillMaxHeight()
+                ) {
+                    TextFieldsForLogin()
+
+                    Spacer(Modifier.height(32.dp))
+
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(getString(R.string.signin).uppercase())
+                    }
+
+                    Spacer(Modifier.height(16.dp))
+
+                    ClickableText(
+                        text = AnnotatedString(getString(R.string.signup)),
+                        onClick = { /*TODO*/ },
+                        style = TextStyle(
+                            textDecoration = TextDecoration.Underline,
+                            color = MaterialTheme.colors.primaryVariant
+                        )
+                    )
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun LoginTitle() {
+        Box(
+            modifier = Modifier.padding(vertical = 128.dp),
+            contentAlignment = Alignment.TopCenter
+        ) {
+            Text(
+                getString(R.string.app_name).uppercase(),
+                style = TextStyle(
+                    color = Color.White,
+                    fontSize = 36.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+        }
+    }
+
+    @Composable
+    fun TextFieldsForLogin() {
+        var username by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+        var isPasswordVisible by remember { mutableStateOf(false) }
+
+        TextField(
+            value = username,
+            label = { Text(text = getString(R.string.username)) },
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = Color.White,
+                cursorColor = Color.Black,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+            ),
+            onValueChange = { username = it },
+            shape = RoundedCornerShape(50.dp),
+            singleLine = true,
+        )
+
+        Spacer(Modifier.height(16.dp))
+
+        TextField(
+            value = password,
+            label = { Text(text = getString(R.string.password)) },
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = Color.White,
+                cursorColor = Color.Black,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+            ),
+            onValueChange = { password = it },
+            visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            shape = RoundedCornerShape(50.dp),
+            singleLine = true,
+            trailingIcon = {
+                val image = if (isPasswordVisible) Icons.Filled.Visibility
+                else Icons.Filled.VisibilityOff
+
+                val description = if (isPasswordVisible) getString(R.string.hide_password)
+                else getString(R.string.show_password)
+
+                IconButton(
+                    onClick = { isPasswordVisible = !isPasswordVisible }
+                ) {
+                    Icon(imageVector = image, description)
+                }
+            }
+        )
     }
 }
