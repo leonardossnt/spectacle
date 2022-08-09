81680a9e97fe628ac4b0bdf0cd449b5259992989
Leonardo Santana <leonardossnt@gmail.com>
2022-08-08 21:50:38 -0400

    Change colors and theming

---
+++ app/src/main/java/tech/ada/leosan/spectacle/CustomColors.kt
@@ -0,0 +1,16 @@
+package tech.ada.leosan.spectacle
+
+import androidx.compose.material.lightColors
+import androidx.compose.ui.graphics.Color
+
+class CustomColors {
+    companion object {
+        val LightPurple = Color(0xffb887e5)
+        val LightBlue = Color(0xff7ab1d4)
+
+        val Colors = lightColors(
+            primary = LightPurple,
+            secondary = LightBlue,
+        )
+    }
+}
