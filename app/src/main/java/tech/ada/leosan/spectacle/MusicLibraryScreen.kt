005c756cd3fec29fa70ad402de2ea3b6e604b0d9
Leonardo Santana <leonardossnt@gmail.com>
2022-08-10 17:27:24 -0400

    Add Mosaic, Title and Add button to MusicLibraryScreen

--- app/src/main/java/tech/ada/leosan/spectacle/MusicLibraryScreen.kt
+++ app/src/main/java/tech/ada/leosan/spectacle/MusicLibraryScreen.kt
@@ -1,18 +1,24 @@
 package tech.ada.leosan.spectacle
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
@@ -33,6 +39,9 @@ fun MusicLibraryScreen(
     ) {
         MusicLibraryTopBar(navController)
         MusicLibrarySearchBar {}
+        MusicLibraryMosaic()
+        MusicLibraryTitle()
+        AddMusicButton()
     }
 }
 
@@ -99,6 +108,55 @@ fun MusicLibrarySearchBar(
             ),
         )
     }
+}
 
+@Composable
+fun MusicLibraryMosaic() {
+    Box(
+        Modifier
+            .padding(32.dp)
+            .size(240.dp)
+            .background(Color.White.copy(alpha = 0.4f))
+    ) {
+        Text("Mosaic here", modifier = Modifier.align(Alignment.Center))
+    }
+}
 
+@Composable
+fun MusicLibraryTitle() {
+    Text(
+        text = stringResource(R.string.my_playlist),
+        style = TextStyle(
+            color = MaterialTheme.colors.onPrimary,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.ExtraBold,
+            textAlign = TextAlign.Center
+        ),
+    )
+}
+
+@Composable
+fun AddMusicButton() {
+    Button(
+        onClick = {  },
+        modifier = Modifier.wrapContentWidth().padding(16.dp),
+        shape = RoundedCornerShape(30.dp),
+        colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.background),
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(2.dp)
+        ) {
+            Icon(
+                Icons.Rounded.Add,
+                contentDescription = null,
+                tint = MaterialTheme.colors.primary,
+            )
+            Spacer(Modifier.width(8.dp))
+            Text(
+                stringResource(R.string.add_new_song),
+            )
+
+        }
+    }
 }
