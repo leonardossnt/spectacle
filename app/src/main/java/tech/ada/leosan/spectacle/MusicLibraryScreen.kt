9a25596f25ca45bc9fe575edc25d5d6422e9234f
Leonardo Santana <leonardossnt@gmail.com>
2022-08-10 16:08:11 -0400

    Add SearchBar to MusicLibraryScreen

--- app/src/main/java/tech/ada/leosan/spectacle/MusicLibraryScreen.kt
+++ app/src/main/java/tech/ada/leosan/spectacle/MusicLibraryScreen.kt
@@ -1,14 +1,15 @@
 package tech.ada.leosan.spectacle
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material.icons.rounded.ArrowBack
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,6 +32,7 @@ fun MusicLibraryScreen(
             .fillMaxSize()
     ) {
         MusicLibraryTopBar(navController)
+        MusicLibrarySearchBar {}
     }
 }
 
@@ -55,4 +57,48 @@ fun MusicLibraryTopBar(
             )
         }
     }
+}
+
+@Composable
+fun MusicLibrarySearchBar(
+    onValueChange: (String) -> Unit,
+) {
+    var searchContent by remember { mutableStateOf("") }
+    Box(
+        modifier = Modifier
+            .padding(horizontal = 48.dp)
+    ) {
+        TextField(
+            value = searchContent,
+            onValueChange = { searchContent = it },
+            placeholder = {
+                Text(
+                    stringResource(R.string.search_placeholder),
+                )
+            },
+
+            // layout
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(50.dp),
+            singleLine = true,
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Outlined.Search,
+                    contentDescription = null
+                )
+            },
+
+            //style
+            colors = TextFieldDefaults.textFieldColors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                leadingIconColor = MaterialTheme.colors.onPrimary,
+                placeholderColor = MaterialTheme.colors.onPrimary.copy(alpha = 0.5f),
+                textColor = MaterialTheme.colors.onPrimary,
+                cursorColor = MaterialTheme.colors.onPrimary
+            ),
+        )
+    }
+
+
 }
