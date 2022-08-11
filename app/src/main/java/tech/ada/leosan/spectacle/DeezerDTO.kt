e659c26ef62db7abfcc0fd524b8d5d6ff6e1ca4a
Leonardo Santana <leonardossnt@gmail.com>
2022-08-11 04:20:18 -0400

    Create Data Transfer Object for Deezer Api

---
+++ app/src/main/java/tech/ada/leosan/spectacle/DeezerDTO.kt
@@ -0,0 +1,37 @@
+package tech.ada.leosan.spectacle
+
+import com.google.gson.annotations.SerializedName
+
+data class DeezerDTO(
+    @SerializedName("data")
+    val data: List<TrackDTO>?,
+)
+
+data class TrackDTO(
+    @SerializedName("title")
+    val trackTitle : String,
+    @SerializedName("artist")
+    val artist : ArtistDTO,
+    @SerializedName("album")
+    val album : AlbumDTO
+)
+
+data class ArtistDTO(
+    @SerializedName("name")
+    val name : String
+)
+
+data class AlbumDTO(
+    @SerializedName("title")
+    val title : String,
+    @SerializedName("cover_medium")
+    val thumbnailUrl : String
+)
+
+fun mapDtoToTrack(dto: TrackDTO) : Track {
+    return Track(
+        title = dto.trackTitle,
+        artist = dto.artist.name,
+        thumbnailUrl = dto.album.thumbnailUrl
+    )
+}
