8da4ab882c7bc301f3f22802ab5971ea71276853
Leonardo Santana <leonardossnt@gmail.com>
2022-08-11 05:30:09 -0400

    Bugfix: thumbnailUrl can be null sometimes

--- app/src/main/java/tech/ada/leosan/spectacle/Track.kt
+++ app/src/main/java/tech/ada/leosan/spectacle/Track.kt
@@ -1,11 +1,10 @@
 package tech.ada.leosan.spectacle
 
 import com.google.gson.annotations.SerializedName
-import java.io.Serializable
 
 data class Track(
     @SerializedName("")
     val title: String = "",
     val artist: String = "",
-    val thumbnailUrl: String = ""
-) : Serializable
+    val thumbnailUrl: String? = ""
+)
